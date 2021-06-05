7a465489a74a54cdf63b85897abae231a112c043
hghimanshu <hghimanshu81@gmail.com>
2021-06-05 12:28:14 +0530

    new file to connect to mongo

--- musicPlayer/db/connect.py
+++ musicPlayer/db/connect.py
@@ -1,9 +1,18 @@
-import MySQLdb
+from pymodm import connect
+import urllib 
+import yaml
+# from cryptography.fernet import Fernet
+import sys
+import os
+from musicPlayer.config import config
 
-def connection():
-    conn = MySQLdb.connect(host="localhost",
-                           user = "root",
-                           passwd = "password",
-                           db = "music_player")
-    c = conn.cursor()
-    return c, conn
+user = config.DB_USER
+password = config.DB_PASSWORD
+
+MONGO_DB_NAME = config.DB_NAME
+MONGO_DB_URL = config.DB_HOST
+MONGOD_URI = 'mongodb://'+user+":"+password+"@"+MONGO_DB_URL+'/'+MONGO_DB_NAME 
+
+# connect to mongo
+connect(MONGOD_URI)
+print('\nINFO: connected to {}'.format(MONGO_DB_NAME))
