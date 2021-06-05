687a0895e6021a1ca74113c8ef6a2dd08bd80b36
hghimanshu <hghimanshu81@gmail.com>
2021-06-05 12:28:02 +0530

    updated the config for db

--- musicPlayer/config/config.py
+++ musicPlayer/config/config.py
@@ -2,8 +2,7 @@ import os
 from os.path import join, dirname, realpath
 
 DEBUG = True
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'password@123'
-MYSQL_DB = 'myMusic'
-MYSQL_CURSORCLASS = 'DictCursor'
+DB_HOST = 'localhost'
+DB_USER = 'root'
+DB_PASSWORD = '123456'
+DB_NAME = 'Flask-Music'
