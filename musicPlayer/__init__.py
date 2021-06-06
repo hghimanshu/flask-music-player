428ac00c62cde02258b460c364880684553f9b21
hghimanshu <hghimanshu81@gmail.com>
2021-06-06 15:39:30 +0530

    changed the init

--- musicPlayer/__init__.py
+++ musicPlayer/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-
+# from flask.ext.session import Session
+from musicPlayer.config import config
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+# Session(app)
 Bootstrap(app)
 
 from musicPlayer import routes
