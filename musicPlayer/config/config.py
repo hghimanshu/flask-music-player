481b4c2f9a3fcc5c7c650eb44e73f36a601e0dd7
hghimanshu <hghimanshu81@gmail.com>
2021-06-06 19:45:21 +0530

    Globalize secret key

--- musicPlayer/config/config.py
+++ musicPlayer/config/config.py
@@ -1,10 +1,13 @@
 import os
 from os.path import join, dirname, realpath
+from cryptography.fernet import Fernet
 
+
+SECRET_KEY = Fernet.generate_key()
 DEBUG = True
 DB_HOST = 'localhost'
 DB_USER = 'root'
 DB_PASSWORD = '123456'
 DB_NAME = 'Flask-Music'
 SESSION_TYPE = 'filesystem'
-SECRET_KEY = '12345678'
+# SECRET_KEY = '12345678'
